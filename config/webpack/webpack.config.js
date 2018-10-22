f6d992d6e8bd523bc736ced8ebdf0f480f443817
Tautvydas Derzinskas <tautvydasuk@gmail.com>
2018-10-22 17:32:27 +0100

    refactor(build): separate source and dist folders (don't keep extension dist in source control)

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,15 +1,13 @@
-const path = require('path');
+const path = require('path')
+  CleanWebpackPlugin = require("clean-webpack-plugin"),
+  CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const OUTPUT_FOLDER = path.resolve(__dirname, '../../extension');
 
 module.exports = {
   entry: path.resolve(__dirname, '../../extension.ts'),
   module: {
     rules: [
-      // {
-      //   test: /\.tsx?$/,
-      //   enforce: 'pre',
-      //   loader: 'tslint-loader',
-      //   exclude: /node_modules/
-      // },
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -22,6 +20,24 @@ module.exports = {
   },
   output: {
     filename: 'extension.js',
-    path: path.resolve(__dirname, '../../extension')
-  }
+    path: OUTPUT_FOLDER
+  },
+  plugins: [
+    new CleanWebpackPlugin(
+      [OUTPUT_FOLDER],
+      { root: path.resolve(__dirname, '../../') }
+    ),
+    new CopyWebpackPlugin([{
+      from: path.resolve(__dirname, '../../src/manifest.json'),
+      transform: function (content, path) {
+        // generates the manifest file using the package.json informations
+        return Buffer.from(JSON.stringify({
+          description: process.env.npm_package_description,
+          version: process.env.npm_package_version,
+          ...JSON.parse(content.toString())
+        }))
+      }
+    }]),
+    new CopyWebpackPlugin([{ from: path.resolve(__dirname, '../../src/icons') }]),
+  ]
 };
