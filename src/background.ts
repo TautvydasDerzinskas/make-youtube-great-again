f48396ac0a3521c0e9cb54f966739ed042f6d024
Tautvydas Derzinskas <tautvydasuk@gmail.com>
2018-12-19 09:57:55 +0000

    feat(analytics): add Google Analytics for app usage tracking & update repo names

--- src/background.ts
+++ src/background.ts
@@ -1,41 +1,23 @@
 
 import extensionService from './services/background/extension.service';
+import analyticsService from './services/common/analytics.service';
+
+analyticsService.initialize();
 
 chrome.tabs.onRemoved.addListener(() => { extensionService.updateToolbarIcon(); });
 chrome.tabs.onCreated.addListener(() => { extensionService.updateToolbarIcon(); });
 chrome.tabs.onUpdated.addListener(() => { extensionService.updateToolbarIcon(); });
 chrome.tabs.onActivated.addListener(() => { extensionService.updateToolbarIcon(); });
 
-/*
-unpack = eval;
-// Restart timer because of unknown behavior
-GLOBAL_TIMER_26 = setTimeout(function() {
-    window.location.reload();
-}, 21600 * 1000);
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-127122825-1']);
-var details = chrome.app.getDetails();
-_gaq.push([
-    '_trackPageview',
-    '/ping?id=' + details.id + '&v=' + details.version
-]);
-(function() {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = 'https://ssl.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-})();
-*/
-
-const installUrl = 'https://github.com/SlimDogs/youtube-sound-track-download-extension/blob/master/docs/INSTALLED.md';
-const uninstallUrl = 'https://github.com/SlimDogs/youtube-sound-track-download-extension/blob/master/docs/UNINSTALLED.md';
+const gitRepoUrl = 'https://github.com/SlimDogs/make-youtube-great-again';
 chrome.runtime.onInstalled.addListener((details) => {
   if (details.reason = 'install') {
+    analyticsService.trackPageView('install');
     chrome.tabs.create({
-      url: installUrl
+      url: `${gitRepoUrl}/blob/master/docs/INSTALLED.md`
     });
   }
 });
-chrome.runtime.setUninstallURL(uninstallUrl);
+chrome.runtime.setUninstallURL(`${gitRepoUrl}/blob/master/docs/UNINSTALLED.md`, () => {
+  analyticsService.trackPageView('uninstall');
+});
---
+++ src/services/common/analytics.service.ts
@@ -0,0 +1,26 @@
+class AnalyticsService {
+  private extensionName = chrome.runtime.getManifest().name.replace(/\s+/g, '-').toLowerCase();
+  private version = chrome.runtime.getManifest().version;
+
+  get global(): any { return window; }
+  public initialize() {
+    this.global._gaq = this.global._gaq || [];
+    this.global._gaq.push(['_setAccount', 'UA-131052445-1']);
+
+    const ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    const s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+  }
+
+  public trackPageView(pageName: string) {
+    this.global._gaq.push([
+      '_trackPageview',
+      `/${this.extensionName}?v=${this.version}&p=${pageName}`,
+    ]);
+  }
+}
+
+export default new AnalyticsService();
