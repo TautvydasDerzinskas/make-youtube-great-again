bbeb8c9e98a89ea07ed47ad96ccddaa2f4a4808c
Tautvydas Derzinskas <tautvydasuk@gmail.com>
2019-05-08 13:47:59 +0100

    fix(Firefox): disable analytics on Firefox browsers (policy issues)

--- src/services/common/analytics.service.ts
+++ src/services/common/analytics.service.ts
@@ -1,28 +1,37 @@
 import browserService from '../common/browser.service';
-import { ApiKeys } from '../../enums';
+import { ApiKeys, Browsers } from '../../enums';
 
 class AnalyticsService {
   private extensionName = chrome.runtime.getManifest().name.replace(/\s+/g, '-').toLowerCase();
   private version = chrome.runtime.getManifest().version;
 
   get global(): any { return window; }
-  public initialize() {
-    this.global._gaq = this.global._gaq || [];
-    this.global._gaq.push(['_setAccount', ApiKeys.Analytics]);
+  get isBrowserNotFirefox() {
+    // Firefox doesn't allow analytics
+    return browserService.browserName !== Browsers.Firefox;
+  }
 
-    const ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = 'https://ssl.google-analytics.com/ga.js';
-    const s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
+  public initialize() {
+    if (this.isBrowserNotFirefox) {
+      this.global._gaq = this.global._gaq || [];
+      this.global._gaq.push(['_setAccount', ApiKeys.Analytics]);
+
+      const ga = document.createElement('script');
+      ga.type = 'text/javascript';
+      ga.async = true;
+      ga.src = 'https://ssl.google-analytics.com/ga.js';
+      const s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(ga, s);
+    }
   }
 
   public trackPageView(pageName: string) {
-    this.global._gaq.push([
-      '_trackPageview',
-      `/${this.extensionName}?v=${this.version}&p=${pageName}&b=${browserService.browserName}`,
-    ]);
+    if (this.isBrowserNotFirefox) {
+      this.global._gaq.push([
+        '_trackPageview',
+        `/${this.extensionName}?v=${this.version}&p=${pageName}&b=${browserService.browserName}`,
+      ]);
+    }
   }
 }
 
