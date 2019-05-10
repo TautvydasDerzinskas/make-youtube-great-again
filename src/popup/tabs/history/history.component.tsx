59c843d81df718a544b179c2c808206cea050940
Tautvydas Derzinskas <tautvydasuk@gmail.com>
2019-05-10 11:33:23 +0100

    refactor(history): exclude disabled features history

--- src/popup/tabs/history/history.component.tsx
+++ src/popup/tabs/history/history.component.tsx
@@ -2,6 +2,7 @@
 
 import * as React from 'react';
 import FeatureStatsGroupComponent from './feature-stats-group/feature-stats-group.component';
+import browserService from '../../../services/common/browser.service';
 
 import { FeaturesMeta } from '../../../features/features';
 
@@ -9,7 +10,7 @@ export default class HistoryComponent extends React.Component {
 
   render() {
     const allGroupsHtml = FeaturesMeta.map((featureMeta) => {
-      if (featureMeta.isInHistoryTab) {
+      if (featureMeta.isInHistoryTab && (!featureMeta.disabledBrowsers || !featureMeta.disabledBrowsers.includes(browserService.browserName))) {
         return <FeatureStatsGroupComponent key={featureMeta.id} meta={featureMeta} />;
       }
     });
